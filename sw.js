09a47eb3fef3499a8b8479622106b14c6c79747a
c0d4 <c8o1d2a@gmail.com>
2026-09-22 18:16:59 +0800

    Darken course text uniformly

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "fakeup-shell-v15";
+const CACHE_NAME = "fakeup-shell-v16";
 const SHELL_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=fakeup-ui-13",
-  "./app.js?v=fakeup-pdf-39",
+  "./styles.css?v=fakeup-ui-14",
+  "./app.js?v=fakeup-pdf-40",
   "./manifest.json",
   "./favicon.ico?v=fakeup-icon-3",
   "./assets/icon-192.png?v=fakeup-icon-3",
